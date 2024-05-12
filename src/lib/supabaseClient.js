277f2d7e1e7bf10a1a4bb94648dee5be2b3cd648
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-12 00:57:40 -0400

    Added protection for credentials

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pwkzjwhwfguhtubedmmv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3a3pqd2h3Zmd1aHR1YmVkbW12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU0NzkzMjAsImV4cCI6MjAzMTA1NTMyMH0.9C_HQQBaEUuVpIOOEA1HwXxlRp2M917KbxwTR2Al0P4';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
