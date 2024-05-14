fa6f0f810e1ece4dca9c04ff3c6c80b35fc8d926
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-14 17:21:26 -0400

    add signout

---
+++ src/app/test/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignOutButton } from "@clerk/nextjs";
+
+const page = () => {
+  return (
+    <div>
+      <SignOutButton />
+    </div>
+  )
+}
+
+export default page
