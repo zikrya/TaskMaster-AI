f2cb2b1ad19ff2da5c2637394135b62dc33586cd
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-14 16:41:40 -0400

    [FEAT] Integrated Clerk Auth

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SignInButton } from '@clerk/nextjs'
+
+const Login = () => {
+  return (
+    <div>
+        <SignInButton />
+
+    </div>
+  )
+}
+
+export default Login
