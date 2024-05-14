65e76a2ca65b91e29e7f8cd15b513a607f7413b4
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-14 18:50:43 -0400

    chore: clean up user logic with navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import NavBar from '../components/navbar'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <NavBar />
     </html>
     </ClerkProvider>
   );
--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
-  '/test(.*)',
+  //'/test(.*)',
 ]);
 
 export default clerkMiddleware((auth, req) => {
