7e07d982c134b6d0388101c0948c154bfe2b8a85
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-14 01:45:49 -0400

    chore: cleaning up

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,4 +1,3 @@
-import AuthButton from "../../components/AuthButton";
 import { createClient } from "../../utils/supabase/server";
 import { redirect } from "next/navigation";
 
@@ -23,7 +22,6 @@ export default async function ProtectedPage() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           </div>
-          <AuthButton />
         </nav>
       </div>
 
@@ -33,19 +31,6 @@ export default async function ProtectedPage() {
         </main>
       </div>
 
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
