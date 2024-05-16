c39056890048014d6406bcc1e8ccf7a6809b61c2
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-16 16:02:00 -0400

    [REFRACTOR] Updated protected route

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
   '/projects(.*)',
-  '/client-board(.*)'
+  '/project(.*)'
 ]);
 
 export default clerkMiddleware((auth, req) => {
