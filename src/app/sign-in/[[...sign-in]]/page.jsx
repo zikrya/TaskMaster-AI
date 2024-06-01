7095cbdd53b8265e250fbb244aad633a66d04b3c
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-31 22:26:16 -0400

    [STYLING] Better layout for clerk auth

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,20 @@
 import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <div>
+        <SignIn
+          path="/sign-in"
+          appearance={{
+            elements: {
+              formButtonPrimary: "bg-slate-500 hover:bg-slate-400 text-sm normal-case",
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
 }
+
--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,16 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp path="/sign-up" />;
+  return (
+<div className="flex items-center justify-center min-h-screen bg-gray-50">
+<div>
+  <SignUp path="/sign-up"
+            appearance={{
+                elements: {
+                  formButtonPrimary: "bg-slate-500 hover:bg-slate-400 text-sm normal-case",
+                },
+              }}/>
+  </div>
+  </div>
+  );
 }
