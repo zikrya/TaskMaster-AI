30e18d7a702287534b35df494518705c23a27a47
zikrya <77081809+zikrya@users.noreply.github.com>
2024-06-07 17:00:22 -0400

    [STYLING] Adjusted user/profile box

--- src/app/user-profile/[[...user-profile]]/page.jsx
+++ src/app/user-profile/[[...user-profile]]/page.jsx
@@ -1,7 +1,9 @@
 import { UserProfile } from "@clerk/nextjs";
 
 const UserProfilePage = () => (
-  <UserProfile path="/user-profile" />
+  <div className="flex items-center justify-center min-h-screen bg-gray-50">
+    <UserProfile path="/user-profile" />
+  </div>
 );
 
 export default UserProfilePage;
