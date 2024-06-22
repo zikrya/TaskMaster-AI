4f3d7445fdd6607cf6ce69aec2554037aed57fe8
zikrya <77081809+zikrya@users.noreply.github.com>
2024-06-22 18:26:11 -0400

    Minor search edits

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,20 @@
 'use client';
 import React from 'react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 const SearchBar = ({ searchQuery, setSearchQuery }) => {
     return (
         <div className="flex justify-center mt-4">
-            <input
-                type="text"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                placeholder="Search tickets..."
-                className="p-1 border rounded-md w-full"
-            />
+            <div className="relative w-full">
+                <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500" />
+                <input
+                    type="text"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    placeholder="Filter by Keyword or by field"
+                    className="pl-10 pr-3 py-1 border rounded-md w-full"
+                />
+            </div>
         </div>
     );
 };
