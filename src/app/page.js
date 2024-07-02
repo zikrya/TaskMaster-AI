a8454e1346ca6a226c3cd9ad5e94fce9cb8361b0
zikrya <77081809+zikrya@users.noreply.github.com>
2024-07-02 03:54:26 -0400

    Updated content

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
           <div className='flex h-full px-6'>
             <div className='w-full md:w-1/2 lg:w-1/3 self-center'>
               <h1 className='text-blue-900 text-4xl font-semibold leading-snug mb-4'>The New Age Project Manager</h1>
-              <p className='text-black text-xl leading-relaxed mb-6'>Introducing DevLiftoff: Revolutionize Your Development Workflow with Instant, Intelligent Task Allocation and Seamless Project Management!</p>
+              <p className='text-black text-xl leading-relaxed mb-6'>DevLiftoff is committed to building the best platform available for understanding development workflows and project management. Instead of the footer. Getting good sounds weird</p>
               <div className='mt-6 flex justify-center md:justify-start'>
                 <Link href="/sign-up" className="w-64 md:w-48 text-white bg-[#4d4cd0] hover:bg-indigo-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Get Started</Link>
               </div>
